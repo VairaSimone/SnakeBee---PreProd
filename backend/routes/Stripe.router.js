22ea8a99af19665bc1d35dc8fe0e6dd2b5fb0d7d
VairaSimone <158765247+VairaSimone@users.noreply.github.com>
2025-08-02 14:33:13 +0200

    test

--- backend/routes/Stripe.router.js
+++ backend/routes/Stripe.router.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import {stripeController, getSessionDetails} from '../controllers/Stripe_controller.js';
+import stripeController from '../controllers/Stripe_controller.js';
 import { authenticateJWT } from '../middlewares/Auth.js';
 
 const stripeRouter = express.Router();
 
 stripeRouter.post('/create-checkout-session', authenticateJWT, stripeController.createCheckoutSession);
-stripeRouter.get('/session/:sessionId', authenticateJWT, getSessionDetails);
+stripeRouter.get('/session/:sessionId', authenticateJWT, stripeController.getSessionDetails);
 
 export default stripeRouter;
