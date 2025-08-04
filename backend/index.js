68020faaf8006f8e1d77e9f225f1c1976e6bc8ba
VairaSimone <158765247+VairaSimone@users.noreply.github.com>
2025-08-04 22:14:44 +0200

    test

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.use(cors({
 }));
 app.use(cookieParser())
 
-app.post('/webhook', express.raw({ type: 'application/json' }), stripeController.stripeWebhook);
+app.post('/api/stripe/webhook', express.raw({ type: 'application/json' }), stripeController.stripeWebhook);
 
 app.use(express.json({ limit: '10kb' }));
 app.use(morgan("dev"))
